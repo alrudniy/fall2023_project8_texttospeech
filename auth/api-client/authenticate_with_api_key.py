f91b5e735fe22136479af0c2d0c2ad97d3022b4f
Richmond0201 <richmondaldouskosasih@gmail.com>
2023-10-25 12:26:37 -0400

    api key instructions update

--- auth/api-client/authenticate_with_api_key.py
+++ auth/api-client/authenticate_with_api_key.py
@@ -14,6 +14,8 @@ def authenticate_with_api_key(quota_project_id: str, api_key_string: str) -> Non
     """
 
     quota_project_id = "text-to-speech-403017"
+    
+    # Change this to your own google cloud token
     api_key_string = "1//011zmC8iCHcTLCgYIARAAGAESNwF-L9IrizotsQdIQZtgMAKCWXIsxv6FZQ1DZCiHwN-vUhIuGC5IN8KgR69vM4uUciiYExFjpeE"
 
     # Initialize the Language Service client and set the API key and the quota project id.
